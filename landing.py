27e9d73bd60895e14d985dcbc8554381e95f742a
abdul-ahad144 <112452139+abdul-ahad144@users.noreply.github.com>
2026-04-06 19:28:47 +0530

    Update landing.py

--- landing.py
+++ landing.py
@@ -1,17 +1,8 @@
 import streamlit as st
 
 def landing_page():
-    st.title("🚀 PragyanAI")
-    st.subheader("Placement Intelligence Engine")
+    st.title("Welcome to PragyanAI")
 
-    st.write("Welcome! Please Login or Register")
-
-    col1, col2 = st.columns(2)
-
-    with col1:
-        if st.button("🔐 Login"):
-            st.session_state.page = "login"
-
-    with col2:
-        if st.button("📝 Register"):
-            st.session_state.page = "register"
+    if st.button("Login"):
+        st.session_state.page = "dashboard"
+        st.session_state.logged_in = True
