7f0885abacdba438583a18031e58d9c859520a1f
abdul-ahad144 <112452139+abdul-ahad144@users.noreply.github.com>
2026-04-06 19:59:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,14 @@ import streamlit as st
 from landing import landing_page
 from dashboard import dashboard
 
-# PAGE CONFIG (sirf yahin hoga)
 st.set_page_config(page_title="PragyanAI", layout="wide")
 
-# SESSION STATE
 if "page" not in st.session_state:
     st.session_state.page = "landing"
 
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
-# ROUTING
 if st.session_state.page == "landing":
     landing_page()
 
