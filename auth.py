dd599d1422792a119db5af3ede754e3063ac5f1b
abdul-ahad144 <112452139+abdul-ahad144@users.noreply.github.com>
2026-04-06 20:01:20 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,32 +1,46 @@
+import pandas as pd
+import os
+
+ADMIN_USER = "admin"
+ADMIN_PASS = "1234"
+
 def load_users():
-    import os
-    import pandas as pd
 
-    ADMIN_USER = "admin"
-    ADMIN_PASS = "1234"
-
-    # File exist nahi karti
     if not os.path.exists("users.csv"):
         df = pd.DataFrame(columns=["username", "password"])
         df.to_csv("users.csv", index=False)
 
-    # File empty hai
     if os.stat("users.csv").st_size == 0:
         df = pd.DataFrame(columns=["username", "password"])
         df.to_csv("users.csv", index=False)
 
-    # Read file
     df = pd.read_csv("users.csv")
 
-    # -----------------------
-    # ADMIN ADD (IF NOT PRESENT)
-    # -----------------------
+    # ADMIN ADD
     if ADMIN_USER not in df["username"].values:
-        admin_row = pd.DataFrame(
-            [[ADMIN_USER, ADMIN_PASS]],
-            columns=["username", "password"]
+        df = pd.concat(
+            [df, pd.DataFrame([[ADMIN_USER, ADMIN_PASS]], columns=["username", "password"])],
+            ignore_index=True
         )
-        df = pd.concat([df, admin_row], ignore_index=True)
         df.to_csv("users.csv", index=False)
 
     return df
+
+
+def register_user(username, password):
+    df = load_users()
+
+    if username in df["username"].values:
+        return False
+
+    new_user = pd.DataFrame([[username, password]], columns=["username", "password"])
+    df = pd.concat([df, new_user], ignore_index=True)
+    df.to_csv("users.csv", index=False)
+
+    return True
+
+
+def login_user(username, password):
+    df = load_users()
+    user = df[(df["username"] == username) & (df["password"] == password)]
+    return not user.empty
